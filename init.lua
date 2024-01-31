474e014266a4902028c6cb2b8bb2d45552d77efa
leet0rz <eskemoporno@hotmail.com>
2024-01-31 09:10:30 +0100

    fixes

--- init.lua
+++ init.lua
@@ -225,7 +225,7 @@ require("lazy").setup({
         build = ":TSUpdate",
         config = function()
             -- ENABLES THIS IF USING WINDOWS:
-            -- require('nvim-treesitter.install').compilers = { 'zig' }
+            -- require('nvim-treesitter.install').compilers = { 'zig' } 
             require('nvim-treesitter.configs').setup {
                 ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
                 -- Install parsers synchronously (only applied to `ensure_installed`)
